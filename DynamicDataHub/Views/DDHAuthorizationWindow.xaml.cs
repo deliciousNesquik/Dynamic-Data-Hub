bfeafdff9fb525b4028f8d365a5899d73421dc99
deliciousNesquik <byte.in4matic@gmai.com>
2024-06-27 22:59:36 +0300

    Создал базовую размету и классы для DDH

---
+++ DynamicDataHub/Views/DDHAuthorizationWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DynamicDataHub.Views
+{
+    public partial class DDHAuthorization : Window
+    {
+        public DDHAuthorization()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+
+        }
+        //public class ConfigurationSettings
+        //{
+        //    public string Title
+        //    {
+        //        get { return ConfigurationManager.AppSettings["Title"] as String; }
+        //    }
+        //}
+    }
+}
---
+++ DynamicDataHub/Views/DDHManagerWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DynamicDataHub.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicDataHub
+{
+    public partial class DDHManager : Window
+    {
+        public DDHManager()
+        {
+            InitializeComponent();
+            var ConnectionWindow = new DDHAuthorization();
+            ConnectionWindow.Show();
+        }
+    }
+}
